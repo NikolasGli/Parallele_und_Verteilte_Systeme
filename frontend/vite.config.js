fab3af9cf465d5ed906e79895d42a6612c69e77d
NikolasGli <niglit00@hs-esslingen.de>
2026-01-22 15:11:09 +0100

    work in Progress

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// vite.config.js
 export default defineConfig({
-  plugins: [vue()],
   server: {
-    port: 5173,
     proxy: {
       '/todos': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
+        target: 'http://localhost:8080', 
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
